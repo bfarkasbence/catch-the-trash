6366561a6bde3a90afc7558bff37cb747411c78a
Farkas Bence <b@LAPTOP-TH590FDE>
2020-05-21 14:30:17 +0200

    add user model, controller and context

---
+++ catch-the-trash/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using catch_the_trash.Models;
+
+namespace catch_the_trash.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<catch_the_trash.Models.User> User { get; set; }
+    }
+}
